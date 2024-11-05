16b1ee1b5c8fda294c586de18f1ce2ef819dfc01
safazi <safazi@airmail.cc>
2024-11-04 20:22:54 -0600

    types

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,7 +1,7 @@
 import { peek, Selector, subscribe } from "@rbxts/charm";
 
 /** Returns a promise that resolves when the result of calling `predicate` is `true` */
-export const promise = (predicate: Selector<boolean>) => {
+export const promise = (predicate: Selector<boolean>): Promise<void> => {
 	if (peek(predicate)) return Promise.resolve();
 
 	return new Promise<void>((resolve, _, onCancel) => {
--- src/utils/watch.ts
+++ src/utils/watch.ts
@@ -1,7 +1,8 @@
 import { peek, Selector, subscribe } from "@rbxts/charm";
+import { Cleanup } from "../types";
 
 /** Like `subscribe` but calls `callback` immediately with the current value. */
-export const watch = <T>(source: Selector<T>, callback: (value: T, prev: T) => void) => {
+export const watch = <T>(source: Selector<T>, callback: (value: T, prev: T) => void): Cleanup => {
 	task.spawn(() => {
 		const state = peek(source);
 		callback(state, state);
