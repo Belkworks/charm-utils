05841f7ef03f32e1b3e0c84cd46a2ec59746d4fb
safazi <safazi@airmail.cc>
2024-11-04 20:28:23 -0600

    types

--- src/utils/tagged.ts
+++ src/utils/tagged.ts
@@ -1,7 +1,9 @@
+import { Selector } from "@rbxts/charm";
 import { signals } from "./signals";
+import { Cleanup } from "../types";
 
 const CollectionService = game.GetService("CollectionService");
 
 /** Returns a selector of all instances tagged with `tag`. */
-export const tagged = (tag: string) =>
+export const tagged = (tag: string): [Selector<Instance[]>, Cleanup] =>
 	signals(() => CollectionService.GetTagged(tag), [CollectionService.TagAdded, CollectionService.TagRemoved]);
