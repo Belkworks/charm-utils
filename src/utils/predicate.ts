92a49af8f70ae38bc10a7e0d5da8e23e6bac1f12
safazi <safazi@airmail.cc>
2024-11-04 20:04:25 -0600

    terms

--- src/utils/predicate.ts
+++ src/utils/predicate.ts
@@ -2,8 +2,8 @@ import { Selector } from "@rbxts/charm";
 
 /** Returns a selector that returns the value of `source` if `predicate` returns `true`, or `undefined` if `false` */
 export const predicate =
-	<T>(source: Selector<T>, predicate: (value: T) => boolean): Selector<T | undefined> =>
+	<T>(source: Selector<T>, predicate: (state: T) => boolean): Selector<T | undefined> =>
 	() => {
-		const value = source();
-		return predicate(value) ? value : undefined;
+		const state = source();
+		return predicate(state) ? state : undefined;
 	};
--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -5,8 +5,8 @@ export const promise = (predicate: Selector<boolean>) => {
 	if (peek(predicate)) return Promise.resolve();
 
 	return new Promise<void>((resolve, _, onCancel) => {
-		const cleanup = subscribe(predicate, value => {
-			if (value) {
+		const cleanup = subscribe(predicate, state => {
+			if (state) {
 				resolve();
 				cleanup();
 			}
