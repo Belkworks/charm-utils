f9056b6ad142faa88db1c8529ce7c7f709b7c082
safazi <safazi@airmail.cc>
2024-11-04 19:48:22 -0600

    simplify

--- src/utils/fallback.ts
+++ src/utils/fallback.ts
@@ -3,7 +3,5 @@ import { Selector } from "@rbxts/charm";
 /** Returns a selector that returns `fallback` if the value of `source` is undefined. */
 export const fallback =
 	<T>(source: Selector<T | undefined>, fallback: T): Selector<T> =>
-	() => {
-		const value = source();
-		return value !== undefined ? value : fallback;
-	};
+	() =>
+		source() ?? fallback;
