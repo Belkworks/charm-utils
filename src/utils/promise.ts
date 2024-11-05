0c8ddb61d6a3a9bc5c27bd581d0a17699754ed21
safazi <safazi@airmail.cc>
2024-11-04 18:38:26 -0600

    document

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,5 +1,6 @@
 import { peek, Selector, subscribe } from "@rbxts/charm";
 
+/** Returns a promise that resolves when the result of calling `predicate` is `true` */
 export const promise = (predicate: Selector<boolean>) => {
 	if (peek(predicate)) return Promise.resolve();
 
