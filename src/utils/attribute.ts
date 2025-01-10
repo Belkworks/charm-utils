a7b70c816c5dc194a765a17d20fa300ab427bb15
safazi <safazics@gmail.com>
2025-01-09 21:18:56 -0600

    remove generic

--- src/utils/attribute.ts
+++ src/utils/attribute.ts
@@ -3,5 +3,5 @@ import { Cleanup } from "../types";
 import { signals } from "./signals";
 
 /** Returns a selector of the `attribute` on `instance`. */
-export const attribute = <T>(instance: Instance, attribute: string): [Selector<AttributeValue | undefined>, Cleanup] =>
+export const attribute = (instance: Instance, attribute: string): [Selector<AttributeValue | undefined>, Cleanup] =>
 	signals(() => instance.GetAttribute(attribute), [instance.GetAttributeChangedSignal(attribute)]);
