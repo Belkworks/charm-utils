32991a90b11988446ed29af0a4f72d4b281b41e6
safazi <safazics@gmail.com>
2025-01-09 21:19:00 -0600

    simplify

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,17 +1,16 @@
 import { peek, Selector, subscribe } from "@rbxts/charm";
 
 /** Returns a promise that resolves when the result of calling `predicate` is `true` */
-export const promise = (predicate: Selector<boolean>): Promise<void> => {
-	if (peek(predicate)) return Promise.resolve();
+export const promise = (predicate: Selector<boolean>): Promise<void> =>
+	peek(predicate)
+		? Promise.resolve()
+		: new Promise<void>((resolve, _, onCancel) => {
+				const cleanup = subscribe(predicate, state => {
+					if (state) {
+						resolve();
+						cleanup();
+					}
+				});
 
-	return new Promise<void>((resolve, _, onCancel) => {
-		const cleanup = subscribe(predicate, state => {
-			if (state) {
-				resolve();
-				cleanup();
-			}
-		});
-
-		onCancel(cleanup);
-	});
-};
+				onCancel(cleanup);
+			});
