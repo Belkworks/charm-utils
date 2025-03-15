d83eca4a8648af254295f1d5805740c148352cdc
safazi <safazics@gmail.com>
2025-03-14 19:51:26 -0500

    watch

--- src/utils/watch.ts
+++ src/utils/watch.ts
@@ -2,11 +2,7 @@ import { peek, Selector, subscribe } from "@rbxts/charm";
 import { Cleanup } from "../types";
 
 /** Like `subscribe` but calls `callback` immediately with the current value. */
-export const watch = <T>(source: Selector<T>, callback: (value: T, prev: T) => void): Cleanup => {
-	task.spawn(() => {
-		const state = peek(source);
-		callback(state, state);
-	});
-
+export const watch = <T>(source: Selector<T>, callback: (value: T, prev?: T) => void): Cleanup => {
+	task.spawn(callback, peek(source));
 	return subscribe(source, callback);
 };
